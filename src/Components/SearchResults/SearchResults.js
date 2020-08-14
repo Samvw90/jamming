7303dda193b889c9f322cc22d69382b2f69a2cc3
Samvw90 <45743681+Samvw90@users.noreply.github.com>
2020-08-14 09:06:46 -0700

    First commit: Jamming React project

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SearchResults.css';
+
+
+class SearchResults extends React.Component {
+
+
+    render() {
+        return(
+            <div className="SearchResults">
+                <h2>Results</h2>
+                Add a TrackList component HERE!
+            </div>
+        );
+    }
+}
+
+export default SearchResults;
